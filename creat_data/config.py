74dcc1404e075eeb774999d3cd990a4ec1c1fc65
renjunxiang <34116367+renjunxiang@users.noreply.github.com>
2018-01-28 05:32:22 +0000

    配置文件

---
+++ creat_data/config.py
@@ -0,0 +1,22 @@
+baidu = {'access_token_url': 'https://aip.baidubce.com/oauth/2.0/token',
+         'api': {
+             'sentiment_classify': {
+                 'Content-Type': 'application/json',
+                 'url': 'https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify'}},
+         'account': {
+             'id_1': {'APP_ID': '10757289',
+                      'API_KEY': 'TGd8CaupdGG8Z0ZOyxoZz04g',
+                      'SECRET_KEY': 'zDCv2coa8WyHE2Iy0goEPZ7sIyKNqLin'},
+             'id_2': {'APP_ID': '000',
+                      'API_KEY': '000',
+                      'SECRET_KEY': '000'}}
+         }
+tencent = {'api': {
+             'nlp_textpolar': {
+                 'url': 'https://api.ai.qq.com/fcgi-bin/nlp/nlp_textpolar'}},
+         'account': {
+             'id_1': {'APP_ID': '000',
+                      'AppKey': '000'},
+             'id_2': {'APP_ID': '000',
+                      'API_KEY': '000'}}
+         }
