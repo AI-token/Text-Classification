942308407b325cbdba162a184ebe87353b0e8eb0
renjunxiang <renjunxiang1215@sina.com>
2018-01-28 13:36:33 +0800

    。

--- creat_data/config.py
+++ creat_data/config.py
@@ -4,9 +4,9 @@ baidu = {'access_token_url': 'https://aip.baidubce.com/oauth/2.0/token',
                  'Content-Type': 'application/json',
                  'url': 'https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify'}},
          'account': {
-             'id_1': {'APP_ID': '10757289',
-                      'API_KEY': 'TGd8CaupdGG8Z0ZOyxoZz04g',
-                      'SECRET_KEY': 'zDCv2coa8WyHE2Iy0goEPZ7sIyKNqLin'},
+             'id_1': {'APP_ID': '000',
+                      'API_KEY': '000',
+                      'SECRET_KEY': '000'},
              'id_2': {'APP_ID': '000',
                       'API_KEY': '000',
                       'SECRET_KEY': '000'}}
